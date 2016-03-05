81ad4ab32e48eac6944f477ea03818331aaac9a0
katiesweet <katie.sweet@hotmail.com>
2016-03-05 12:19:34 -0700

    milestone 2 changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var til = require('./routes/til');
+var entries = require('./routes/entries');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // }));
 
 app.use('/', routes);
-app.use('/til', til);
+app.use('/entries', entries);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/til.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET til listing. */
-router.get('/', function(req, res, next) {
-  res.render('til/index', { title: 'Today I Learned' });
-});
-
-module.exports = router;
